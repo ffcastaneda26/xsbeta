8ff914b333dd091a69ccd9765a4e883618c750df
Federico Castañeda <ffcastaneda@gmail.com>
2025-05-03 10:38:34 -0600

    Agregar company_id a tablas: permissions y role_permissions

---
+++ database/migrations/2025_05_03_163310_add_company_id_to_role_permissions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Company;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('role_permissions', function (Blueprint $table) {
+            $table->foreignIdFor(Company::class)->nullable()->comment('Empresa');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('role_permissions', function (Blueprint $table) {
+            $table->dropForeign(['company_id']);
+            $table->dropColumn('company_id');
+        });
+    }
+};
